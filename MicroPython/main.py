4a83bd2373bb0fe4a3c6a0eabc81ac1a0ff0469a
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-15 17:12:59 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Liya Getachew
+Created on: Sep 2023
+This module shows "Hello, MTHS!" on micro:bit.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Hello, MTHS!")
